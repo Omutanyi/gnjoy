721eac11dcdcc72d7feced2266880cdfb4055649
Javier Omutanyi <javieromutanyi@gmail.com>
2021-02-15 15:38:05 +0300

    Frontend initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import Club from 'gnjoy/src/screens/Club.js';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          <Club />
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
---
+++ src/screens/Club.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class Club extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Club</Text>
+      </View>
+    );
+  }
+}
+
+export default Club;
